7a539616170f03972976c0ee586e82d22383d046
alexanderJr <alexmarcjr21@gmail.com>
2024-01-24 21:39:37 +0000

    change en string

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,24 @@
 import { LanguageProvider } from "./locales/index";
-// import styles from "./App.module.css";
 import { About } from "./components/About/About";
 import { Contact } from "./components/Contact/Contact";
 import { Experience } from "./components/Experience/Experience";
 import { Hero } from "./components/Hero/Hero";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Projects } from "./components/Projects/Projects";
-import styles from "./App.module.css"
+import styles from "./App.module.css";
 
 function App() {
   return (
     <LanguageProvider>
-	<div className={styles.App}>
+      <div className={styles.App}>
         <Navbar />
         <Hero />
         <About />
         <Experience />
         <Projects />
         <Contact />
-		</div>
+      </div>
     </LanguageProvider>
-      // </div>
   );
 }
 
